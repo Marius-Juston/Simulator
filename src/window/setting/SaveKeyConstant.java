31b0816d8f1c593612f9fc040cc2aec5ab1dee4b
NamelessSuperCoder <Marius.juston@hotmail.fr>
2017-08-06 19:20:24 -0400

    Added Name Constants for the Settings

---
+++ src/window/setting/SaveKeyConstant.java
@@ -0,0 +1,14 @@
+package window.setting;
+
+
+class SaveKeyConstant {
+	static final String CHECKPOINT_KEY = "Checkpoint save path";
+	static final String MAP_KEY = "Map save path";
+	static final String CHECKPOINT_FREQUENCY_KEY = "Checkpoint save frequency";
+	static final String RANDOM_SEED_KEY = "Random seed";
+	static final String DIMENSION_KEY = "Game dimension";
+	static final String N_OBJECTS_KEY = "Number of GameObject at once";
+	static final String GRAVITY_KEY = "Gravity";
+}
+
+
